1787e1444f74f43a16614c51583c7abe74ec9740
Al Snow <jasnow@hotmail.com>
2023-09-20 10:46:07 -0400

    Upgraded gem(s): [actioncable,actionmailbox,actionmailer,actionpack,actiontext,actionview,activejob,activemodel,activerecord,activestorage,activesupport,base64,bigdecimal,config.active_record.legacy_connection_handling,connection_pool,drb,erubi,io-console,irb,method_source,mini_mime,mutex_m,net-smtp,nokogiri,rack,rack-session,rackup,rails,rails-dom-testing,rails-html-sanitizer,railties,rdoc,reline,thor,timeout,webrick,zeitwerk]

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Myapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.active_record.legacy_connection_handling = false
+    config.active_support.cache_format_version = 7.0
   end
 end
